8cb42878985111b90392cb8ea22add806cf5607c
eyvonne <eyvonne@users.noreply.github.com>
2020-07-29 15:51:10 -0700

    catching up github

---
+++ WhosTalkingBack/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from .models import *
+
+load_dotenv()
+POSTGRES = {
+    'user': os.environ['user'],
+    'pw': os.environ['pw'],
+    'db': os.environ['db'],
+    'host': os.environ['host'],
+    'port': '5432'
+}
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+            %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['DEBUG'] = True
+
+    DB.init_app(app)
+
+    @app.route('/')
+    def home_route():
+        DB.session.add(User(display_name='example', gender='female',
+                            ethnicity='white', other=None))
+        DB.session.commit()
+
+        return f'{User.query.all()}'
+
+    return app
---
+++ WhosTalkingBack/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+
+class User(DB.Model):
+    ''' users for the app, contains basic demographic information'''
+    id = DB.Column(DB.Integer, primary_key=True)
+    display_name = DB.Column(DB.Text)
+    gender = DB.Column(DB.Text)
+    ethnicity = DB.Column(DB.Text)
+    other = DB.Column(DB.Text)
